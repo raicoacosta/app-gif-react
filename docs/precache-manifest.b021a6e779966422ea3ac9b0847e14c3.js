911c1d15dee87175ea4c9056bc1abf7a4b71cea7
Raico Acosta <raico26@gmail.com>
2020-10-09 16:20:46 -0500

    doc agregado

---
+++ docs/precache-manifest.b021a6e779966422ea3ac9b0847e14c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4d5ea551b5d392c7f4bb996df31c76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d21921db0fce90ec3c7c",
+    "url": "/static/css/main.8ec3880c.chunk.css"
+  },
+  {
+    "revision": "de701a9a0ad452e2a193",
+    "url": "/static/js/2.ab7aee1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab7aee1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21921db0fce90ec3c7c",
+    "url": "/static/js/main.20af023a.chunk.js"
+  },
+  {
+    "revision": "ce857019487f3f7f237a",
+    "url": "/static/js/runtime-main.643fc342.js"
+  }
+]);
